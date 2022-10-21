ae069e75a0f700b843fc992f48a5a941f566bd6f
SSIIRRPP <jorge.sirp@gmail.com>
2022-10-21 15:48:39 +0200

    CodeShower modified

--- src/components/CodeShower.js
+++ src/components/CodeShower.js
@@ -1,23 +1,13 @@
 import PropTypes from "prop-types";
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import js from "react-syntax-highlighter/dist/esm/languages/hljs/javascript";
-import ocean from "react-syntax-highlighter/dist/esm/styles/hljs/ocean";
+import Highlight from "react-highlight";
 import "./styles/CodeShower.scss";
-
-SyntaxHighlighter.registerLanguage("javascript", js);
 
 const CodeShower = ({ children, className, id, title, text, file }) => {
   return (
     <code-shower class={`CodeShower ${className}`} id={id} is="custom">
       {title ? title : null}
       {text ? text : null}
-      <SyntaxHighlighter
-        customStyle={{ width: "95%" }}
-        language="javascript"
-        style={ocean}
-      >
-        {file ? file : children}
-      </SyntaxHighlighter>
+      <Highlight language="javascript">{file ? file : children}</Highlight>
     </code-shower>
   );
 };
